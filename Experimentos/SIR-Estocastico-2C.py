a50ec19de4cfec3e84858c43ed6e502e4a7b2ebf
fernando <fdmazzone@gmail.com>
2020-07-26 10:59:18 -0300

    Mdelo estocástico dos ciudades

---
+++ Experimentos/SIR-Estocastico-2C.py
@@ -0,0 +1,190 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 17 16:21:00 2020
+
+@author: Fernando Mazzone
+"""
+def SIR_2C_estocastico(N1,N2,n,R01,R02,media_transf,delta_s):
+    """
+    Dos ciudades sobre las que avanza una epidemia. En cada una de ellas se
+    asume un modelos SIR discreto. Se asume que en cada iteración con 
+    probabilidad p un individuo de una ciudad puede pasar a la otra.
+    La primera ciudad con un número de reproducción básico R01 y la segunda 
+    con R02. La unidad de tiempo es el periodo de infectividad y la población
+    total está normalizada N=1.
+    Parametros:
+        n: int (#iteraciones)
+        R01: float64,  número de reproducción básico ciudad 1
+        R02: float64,  número de reproducción básico ciudad 2
+        p: float64, probabilidad que se intercambie Delta_H habitantes
+           entre ciudades
+        Delta_H: float 64 algo así como un individuo
+    """
+    
+    x0=np.array([[N1-10, N2 ],[10,0],[0,0]],dtype=int)
+    
+    ## delta_s son los incrementos de tiempos. 
+    ##Debe ser elegido
+    ##               delta_s<min{1,1/max{R01,R02}}
+    
+    beta01,beta02=R01/N1,R02/N2
+    
+    
+    
+    X=np.zeros([3,2,n],dtype=int)
+    X[:,:,0]=x0
+    Transfer=np.zeros([3,2,n-1],dtype=int)
+    I1d=np.zeros(n-1,dtype=int)
+    I2d=np.zeros(n-1,dtype=int)
+    
+    for i in range(n-1):
+        X0,I1d[i],I2d[i],T0=SIR_est_2C(X[:,:,i],beta01,beta02,media_transf,delta_s)
+        X[:,:,i+1]=X0
+        Transfer[:,:,i]=T0
+
+    return X,I1d,I2d,Transfer
+
+
+
+def SIR_est_2C(x,beta01,beta02,media_transf, delta_s):
+    
+    
+    ## Decidir cuantos habitantes pasan de una población a otra
+    """
+    S1,I1,R1=x[:,0]
+    S2,I2,R2=x[:,1]
+
+    
+    N1=float(S1+I1+R1)
+    N2=float(S2+I2+R2)
+    """
+    
+    N1=float(np.sum(x[:,0]))
+    N2=float(np.sum(x[:,1]))
+    
+    Transfer1=poisson(delta_s*media_transf*x[:,0]/N1)
+    Transfer2=poisson(delta_s*media_transf*x[:,1]/N2)
+ 
+    Transfer1=np.minimum(Transfer1,x[:,0])
+    Transfer2=np.minimum(Transfer2,x[:,1])
+    
+    Transfer=np.vstack((Transfer1,Transfer2)).T
+    
+    
+    x[:,0]=Transfer2-Transfer1+x[:,0]
+    x[:,1]=Transfer1-Transfer2+x[:,1]
+    S1,I1,R1=x[:,0]
+    S2,I2,R2=x[:,1]
+    
+    
+    """
+    mu1=beta01*delta_s*S1*I1
+    mu2=delta_s*I1
+    nu1=beta02*delta_s*S2*I2
+    nu2=delta_s*I2
+    """
+    mu1=beta01*delta_s*S1
+    mu2=delta_s
+    nu1=beta02*delta_s*S2
+    nu2=delta_s   
+    
+    
+    I1_dot,R1_dot=np.sum(poisson(lam=(mu1,mu2),size=(I1,2)),axis=0)
+    I2_dot,R2_dot=np.sum(poisson(lam=(nu1,nu2),size=(I2,2)),axis=0)
+
+    Delta=np.array([[I1_dot,I2_dot],[R1_dot,R2_dot]])
+
+    
+    Delta=np.minimum(Delta,x[:2,:2])
+    
+    
+
+    S1_new=S1-Delta[0,0]
+    I1_new=I1-Delta[1,0]+Delta[0,0]
+    R1_new=R1+Delta[1,0]
+    
+    S2_new=S2-Delta[0,1]
+    I2_new=I2-Delta[1,1]+Delta[0,1]
+    R2_new=R2+Delta[1,1]
+    
+   
+    
+    X=np.array([[S1_new,S2_new ],[I1_new,I2_new],[R1_new,R2_new]])
+    return X,I1_dot,I2_dot, Transfer
+
+
+
+
+from numpy.random import rand, binomial, multinomial, poisson
+
+
+###################  EXPERIMENTO #############################################
+##############################################################################
+
+fig, ((ax1,ax2,ax3),(ax4,ax5,ax6))=plt.subplots(2,3,figsize=(20,15))
+
+R01,R02=1.1,5.1
+
+
+
+N1=100000
+N2=100000
+delta_s=.1*min(1,N1/R01,N2/R02)
+media_transf=10
+n=1000
+
+
+X=np.zeros([3,2,n],dtype=int)
+T=np.zeros([3,2,n-1],dtype=int)
+I1d=np.zeros(n-1,dtype=int)
+I2d=np.zeros(n-1,dtype=int)
+
+nro_exp=1
+for j in range(nro_exp):
+    X1,I1d1,I2d1,T1=SIR_2C_estocastico(N1,N2,n,R01,R02,media_transf,delta_s)
+    X+=X1
+    T+=T1
+    I1d+=I1d1
+    I2d+=I2d1
+X=X/nro_exp
+T=T/nro_exp
+I1d=I1d/nro_exp
+I2d=I2d/nro_exp
+
+
+
+s=np.arange(0,iteraciones*delta_s,delta_s)
+
+
+
+
+
+ax1.plot(s,X[:,0,:].T,marker='o')
+ax1.set(yscale='log',ylim=(1,N1+N2))
+ax1.set_title('Ciudad 1')
+
+
+ax2.plot(s,X[:,1,:].T,marker='o')
+ax2.set(yscale='log',ylim=(1,N1+N2))
+ax2.set_title('Ciudad 2')
+
+Y=np.sum(X,axis=1) ## Suma de las dos ciudades
+Ia=np.sum(Y[1:,:],axis=0) ## Infectados acumulados
+Iad=np.diff(Ia)  ## infectados diarios
+
+###  Grafica de infectados acumulados###################################
+
+ax3.plot(s,Ia,s[1:],Iad)
+ax3.set(yscale='log',ylim=(1,N1+N2))
+ax3.set_title('Pais')
+
+ax4.plot(s[:-1],T[:,0,:].T.cumsum(axis=0),marker='o')
+ax4.set_title('Ciudad 1-Migraciones')
+
+ax5.plot(s[:-1],T[:,1,:].T.cumsum(axis=0),marker='o')
+ax5.set_title('Ciudad 2-Migraciones')
+
+ax6.plot(s[:-1],I1d,s[:-1],I2d,marker='o')
+ax6.set_title('Ciudadades 1 y 2-Casos nuevos')
+
--- programas/Graficos.py
+++ programas/Graficos.py
@@ -9,44 +9,184 @@ import pandas as pd
 
 
 ########### Ejemplo 1, Grafico area de casos diarios y muertes###############
-Data=pd.read_csv('Data/Epidemic/Covid19Casos.csv')
-Data=Data[Data.residencia_provincia_nombre=='Córdoba']
-I1=Data.clasificacion_resumen=='Confirmado'
-I2=Data.fallecido=='SI'
-DataM=Data[I1 & I2]
-DataC=Data[I1]
-J0=DataC[['id_evento_caso','fecha_apertura']].groupby('fecha_apertura')
-J1=J0.count().rename(columns={'id_evento_caso':'confirmado_diario'})
-H0=DataM[['id_evento_caso','fecha_fallecimiento']].groupby('fecha_fallecimiento')
-H1=H0.count().rename(columns={'id_evento_caso':'muertes_diarias'})
-H1.index=pd.to_datetime(H1.index)
-J1.index=pd.to_datetime(J1.index)
-fig, axs = plt.subplots(figsize=(8,8))
-plt.yscale('log')
-J1.plot(ax=axs,Marker='o')
-H1.plot(ax=axs,Marker='o')
+
+def EpiArg(provincia='Córdoba', dpto=None):
+    Data=pd.read_csv('Data/Epidemic/Covid19Casos.csv')
+    if not provincia=="Todas":
+        Data=Data[Data.residencia_provincia_nombre==provincia]
+    if not dpto==None:
+        Data=Data[Data.residencia_departamento_nombre==dpto]
+    DataC=Data[Data.clasificacion_resumen=='Confirmado']
+    DataM=DataC[DataC.fallecido=='SI']
+    
+    J0=DataC[['id_evento_caso','fecha_apertura']].groupby('fecha_apertura')
+    J1=J0.count().rename(columns={'id_evento_caso':'confirmado_diario'})
+    H0=DataM[['id_evento_caso','fecha_fallecimiento']].groupby('fecha_fallecimiento')
+    H1=H0.count().rename(columns={'id_evento_caso':'muertes_diarias'})
+    H1.index=pd.to_datetime(H1.index)
+    J1.index=pd.to_datetime(J1.index)
+    
+    H2=H1.cumsum().rename(columns={'muertes_diarias':'muertes_acumuladas'})
+    J2=J1.cumsum().rename(columns={'confirmado_diario':'confirmados'})
+    
+    fig, ((ax1,ax2,ax3),(ax4,ax5,ax6)) = plt.subplots(2,3,figsize=(18,12))
+    ax1.set_yscale('log')
+    J2.plot.area(ax=ax1,legend=True)
+    J1.plot.area(ax=ax1,legend=True)
+    H2.plot.area(ax=ax1,legend=True)
+    H1.plot.area(ax=ax1,legend=True)
+    if dpto==None:
+        ax1.set_title(unicode(provincia,"utf-8"),fontsize=18)
+    else:
+        ax1.set_title(unicode(provincia,"utf-8")+'-'+unicode(dpto,"utf-8")
+        ,fontsize=18)
 
 
-############# Ejemplo 2 Histogramas edades
-DataM.hist('edad',bins=range(100))
-
-#########3###Ejemplo 3, diagrama torta de sexos
-DataM.sexo.value_counts().plot.pie()
-
-
-
-#############  Ejemplo 4, Analisis de edad media muertes
-fig3, axs3 = plt.subplots(figsize=(8,8))
-DataM_g=DataM.groupby('fecha_fallecimiento').mean()
-DataM_g.edad.plot(ax=axs3)
-DataM_g.edad.rolling(20, center=True).mean().plot(ax=axs3)
-
+    ############# Ejemplo 2 Histogramas edades
+    DataM.hist('edad',ax=ax2,bins=range(100))
+    ax2.set_title(u"Distribución edad muertes",fontsize=18)
+    #########3###Ejemplo 3, diagrama torta de sexos
+    DataM.sexo.value_counts().plot.pie(ax=ax3)
+    ax3.set_title(u"Distribución muertes por sexo",fontsize=18)
+    
+    
+    #############  Ejemplo 4, Analisis de edad media muertes
+    
+    DataM_g=DataM.groupby('fecha_fallecimiento').mean()
+    DataM_g.edad.plot(ax=ax4)
+    DataM_g.edad.rolling(20, center=True).mean().plot(ax=ax4)
+    ax4.set_title(u"Evolución edad media muertes",fontsize=18)
 ########## Ejemplo 5. Grafico area confirmados, 
 ### confirmados diarios y muertes globales
-DataC, DataM, DataR=readData('Denmark')
-DataCd=DataC.diff().apply(abs).rename('Confirmados diarios')
-fig,ax=plt.subplots(figsize=(8,8))
-plt.yscale('log')
-DataC.plot.area(ax=ax,legend=True)
-DataCd.plot.area(ax=ax,legend=True)
-DataM.plot.area(ax=ax,legend=True)
+
+def EpiGlobal(Pais='Tierra'):
+    DataC, DataM, DataR=readData(Pais)
+    DataCd=DataC.diff().apply(abs).rename('Confirmados diarios')
+    fig,ax=plt.subplots(figsize=(8,8))
+    plt.yscale('log')
+    DataC.plot.area(ax=ax,legend=True)
+    DataCd.plot.area(ax=ax,legend=True)
+    DataM.plot.area(ax=ax,legend=True)
+    DataM.diff().apply(abs).rename('muertes_diarias').plot.area(ax=ax,legend=True)
+    ax.set_title(unicode(Pais,"utf-8"),fontsize=18)
+    
+    
+    
+def MapaCOVID(provincia):
+    """
+        Hace un mapa de la provincia dividida por departamentos y colorea con 
+        intensidades acorde a la cantidad de infectados totales de cada 
+        departamento. 
+    """
+    
+    campo='confirmado'
+    
+    
+    if provincia=="AMBA":
+        filepath4=dir1+"/Data/GeoData/AMBA.json"
+        MapaProv=geopandas.read_file(filepath4)
+        MapaProv.insert(9, "MuertesxInfectado", 0.0)
+        MapaProv.index=MapaProv.in1
+        DataProv=readDataArg('AMBA',campo=campo)       
+    else:
+        Arg=geopandas.read_file(filepath2)
+        CodProv=str(codigo.Cod[provincia]).zfill(2)
+        MapaProv=Arg[[h[:2]==CodProv for h in Arg['in1']]]
+        MapaProv.insert(9, "MuertesxInfectado", 0.0)#isertar columna nueva
+        MapaProv.index=MapaProv.in1
+        DataProv=readDataArg(provincia,campo=campo)
+        
+    if len(DataProv)==0:
+        return "No se registra"
+    if provincia=="AMBA":
+        CasosCABA=DataProv[DataProv.residencia_provincia_nombre=='CABA']
+        Total=float(len(CasosCABA))
+        H=CasosCABA.fallecido.value_counts()
+        MapaProv.at['02',"MuertesxInfectado"]=float(H['SI'])/Total*100
+        
+        
+        
+        
+        DataProv=DataProv[DataProv.residencia_provincia_id==6]
+        DataProvM=DataProv[DataProv.fallecido=='SI']
+        
+        
+        DataI=DataProv.residencia_departamento_id.value_counts()
+        
+        DataIM=DataProvM.residencia_departamento_id.value_counts()
+
+        
+        for h in DataIM.index:
+            #if not(h==0):
+            id='06'+str(h).zfill(3)
+            MapaProv.at[id,"MuertesxInfectado"]=float(DataIM[h])/float(DataI[h])*100
+    else:
+        DataI=DataProv.residencia_departamento_id.value_counts()
+        DataProvM=DataProv[DataProv.fallecido=='SI']
+        DataIM=DataProvM.residencia_departamento_id.value_counts()
+        for h in DataIM.index:
+            #if not(h==0):
+            id=CodProv+str(h).zfill(3)
+            print(id)
+            MapaProv.at[id,"MuertesxInfectado"]=float(DataIM[h])/float(DataI[h])*100
+        
+    fig, ax = plt.subplots(1, 1)
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("right", size="5%", pad=0.1)
+    MapaProv.plot(column='MuertesxInfectado',ax=ax,cax=cax,edgecolor="black",cmap=newcmp , legend=True)  
+    ax.set_title(unicode(provincia,"utf-8"),fontsize=26)
+    
+    fig1, ax1 = plt.subplots(1, 1)
+    
+    Resultado=MapaProv.sort_values("MuertesxInfectado",ascending=False)
+    Resultado.MuertesxInfectado.plot.bar(ax=ax1)
+    return Resultado.nam
+
+def readDataArg(provincia, campo=None):
+    Data=pd.read_csv(filepath1)
+    if campo=='confirmado':
+        Data=Data[Data.clasificacion_resumen=="Confirmado"]
+    if provincia=='AMBA':
+        DataAMBA=geopandas.read_file(dir1+'/Data/GeoData/AMBA.json')
+        #Primer datos es CABA lo leere de otra forma
+        I1=[int(i[2:]) for i in DataAMBA.in1.values[1:]]
+        I2=Data['residencia_provincia_id']==6
+        I3=Data['residencia_departamento_id'].isin(I1)
+        I4=Data['residencia_provincia_id']==2
+        I= (I3 & I2) | I4 
+
+        DataProv=Data.loc[I]
+    else:
+        CodProv=codigo.Cod[provincia]
+        I=Data.residencia_provincia_id==CodProv 
+        DataProv=Data[I] 
+    return DataProv
+
+
+#################  LIBRERIAS   ###############################################
+import pandas as pd
+import geopandas
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+import os
+from matplotlib import cm
+from matplotlib.colors import ListedColormap
+import numpy as np
+
+#############  Nueva COLOR MAP ###############33
+viridis = cm.get_cmap('Greens', 4096)
+newcolors = viridis(np.linspace(0, 1, 4096))
+white = np.array([.0, .0, .0, .0])
+newcolors[:1, :] = white
+newcmp = ListedColormap(newcolors)
+
+##################  DIRECCIONES DE CARPETAS ###################################
+dir1=unicode(os.getcwd(),'utf-8')
+filepath1=dir1+"/Data/Epidemic/Covid19Casos.csv"
+filepath2=dir1+"/Data/GeoData/departamento.json"
+filepath3=dir1+"/Data/GeoData/provincia.json"
+
+
+######################  CODIGOS DE provincia ##################################
+codigo=pd.read_csv(dir1+'/Data/GeoData/CodProv.csv')
+codigo.index=codigo.Provincia
